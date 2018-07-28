5de1efe31e3455c4baa329138820c2db88609d07
Peter Parianos <gugshy123@yahoo.com>
2018-07-27 23:27:34 -0400

    compute the wrong type of sum --- not tested

--- Assign2/threads.c
+++ Assign2/threads.c
@@ -7,18 +7,25 @@
 
 //these are shared by all threads
 //pthread_t tid[2];
-double start_points[4];
+int start_points[4];
 double grand_sum=0.0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 //this holds information for each newly created thread, excluding the main thread
 struct thread_info{
-    pthread_t tid2;
+    pthread_t tid;
     int threadnum;
     int index;    
 };
 void sharedResource(int threadnum, int index){
-    double sum = sqrt(start_points[index])+sqrt(start_points[index+1]);
+    double sum;
+    int start = start_points[index];
+    int end = start_points[index+1];
+    while(start <= end){
+        sum += sqrt(start);
+        start++;
+    }
+    
     pthread_mutex_lock(&lock);
     printf("\nThread %d running", threadnum);
 	grand_sum += sum;
@@ -37,7 +44,6 @@ int main(int argc, char *argv[]){
 	// we need to collect argument and convert to integer
 	int error, n, i=0;    
     n = atoi(argv[1]);
-    n = (double)n;
     
     //initialize lock
     if(pthread_mutex_init(&lock,NULL) != 0){
@@ -56,7 +62,7 @@ int main(int argc, char *argv[]){
 	while(i<2){
         tinfo[i].threadnum = i +1; 
         tinfo[i].index = i +1; 
-        error = pthread_create(&tinfo[i].tid2,NULL,&do_partial, &tinfo[i]);
+        error = pthread_create(&tinfo[i].tid,NULL,&do_partial, &tinfo[i]);
         if(error != 0)
             perror("pthread_create");
         i++;	
@@ -64,8 +70,8 @@ int main(int argc, char *argv[]){
     
     sharedResource(0,0); // main thread accesses shared resource 
     //join threads 
-    pthread_join(tinfo[0].tid2, NULL);
-    pthread_join(tinfo[1].tid2, NULL);
+    pthread_join(tinfo[0].tid, NULL);
+    pthread_join(tinfo[1].tid, NULL);
     pthread_mutex_destroy(&lock);
     
     printf("\nThe grand sum is: %f\n", grand_sum);
